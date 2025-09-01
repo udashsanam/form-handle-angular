623c79b59f1c9f6321e5b96231da0cd28b25f043
udashsanam <udashsanam@gmail.com>
2025-09-01 10:43:22 +0545

    add: form control using reative form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.form);
+    console.log(this.form.controls.email.value);
+    console.log(this.form.controls.password.value)
+  }
+
+}
