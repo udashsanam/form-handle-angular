ab8242dca1e4a3931733a82bbe1d3f9155b1d789
udashsanam <udashsanam@gmail.com>
2025-09-01 16:49:08 +0545

    add: validation for sign up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email : new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.min(6)]
+    })
+  })
+
+  onSubmit() {
+    console.log(this.form);
+    console.log(this.form.controls.email.value);
+    console.log(this.form.controls.password.value)
+  }
+  get passwordIsInvalid(){
+    return (this.form.controls.password.invalid && this.form.controls.password.dirty
+    && this.form.controls.password.touched);
+  }
+}
